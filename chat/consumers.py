8fcd2db56df941629c5be643f977cd1e6646fd99
Thomas <tdenzer05@gmail.com>
2020-09-21 16:05:39 -0700

    My Site app, experimenting with auth_user

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,11 +3,13 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
         
+    
+
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
         
-        self.username = self.scope["user"]
+        #self.username = self.scope["user"]
         
         # Join room goup
         await self.channel_layer.group_add(
--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 from .models import Room
 
 def index(request):
 	room_list = Room.objects.order_by('title')
-	return render(request, 'chat/index.html', {'room_list' : room_list})
+	return render(request, 'chat/index.html', {'room_list': room_list})
 
 def room(request, room_name):
-	return render(request, 'chat/room.html', { 'room_name': room_name})
+	return render(request, 'chat/room.html', {'room_name': room_name})
