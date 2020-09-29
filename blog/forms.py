502e7a6e57461e45484f4efbda5221525cb6b84f
Thomas <tdenzer05@gmail.com>
2020-09-28 20:10:07 -0700

    My Site app, page confirmation delete successful, allow author only to edit post.

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 from .models import Post
 
@@ -7,3 +8,15 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class DeleteForm(forms.Form):
+    YES_NO_CHOICES = [
+    (True, 'Yes'),
+    (False, 'No'),
+    ]
+
+    yes_no = forms.ChoiceField(
+            widget=forms.RadioSelect(attrs={'class': "custom-radio-list"}),
+            label='',choices=YES_NO_CHOICES)
+    
+    
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.utils import timezone
 
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, DeleteForm
 
 def post_list(request):
 	
@@ -33,27 +33,59 @@ def post_new(request):
 def post_edit(request, pk):
 	
 	post = get_object_or_404(Post, pk=pk)
-		
-	if request.method == "POST":
-		form = PostForm(request.POST, instance=post)
-		if form.is_valid():
-			post = form.save(commit=False)
-			post.author = request.user
-			post.published_date = timezone.localtime(timezone.now())
-			post.save()
-			return redirect('post_detail', pk=post.pk)
+	
+	if post.author == request.user:	
+		if request.method == "POST":
+			form = PostForm(request.POST, instance=post)
+			if form.is_valid():
+				post = form.save(commit=False)
+				post.author = request.user
+				post.published_date = timezone.localtime(timezone.now())
+				post.save()
+				return redirect('post_detail', pk=post.pk)
+		else:
+			form = PostForm(instance=post)
 	else:
-		form = PostForm(instance=post)
+		"""
+		return render(request, 'blog/post_detail.html', {
+                'post': post,
+                'error_message': "You are not the post author.",
+            })
+        """
+		return HttpResponse("You are not the post author.", content_type="text/plain")
+
 	return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_delete(request, pk):
 	
 	post_to_delete = get_object_or_404(Post, pk=pk)
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-
+	
+	# process the form data
+	if request.method == 'POST':
+		# create a form instance and populate it with data from the request
+		form = DeleteForm(request.POST)
+		# check whether it's valid
+		if form.is_valid():
+			# access the form data
+			selection = form.cleaned_data['yes_no']
+			# check if 'Yes' condition
+			if selection == 'True':
+				# delete the post
+				Post.objects.filter(pk=pk).delete()
+				return redirect('/blog/')
+			# if 'No' condition
+			else:
+				# send to blog index page
+				return redirect('/blog/')
+	else:
+		form = DeleteForm()	
+	return render(request, 'blog/post_delete.html', {'form': form})
+	
+	"""
 	if post_to_delete.author == request.user:
 		Post.objects.filter(pk=pk).delete()
 	else:
-		return HttpResponse("This post was not created by you.", content_type="text/plain")
-
+		return HttpResponse("You are not the post author.", content_type="text/plain")
 	return render(request, 'blog/index.html', {'posts':posts})
+	"""
